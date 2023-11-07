f1166bb5e2c8c8f5dc40384eb2c5c9482f531f83
Tiffane_083506 <tiffanessouza@gmail.com>
2023-11-07 08:25:46 -0300

    esportes2e

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,19 @@ public class Main {
         // Encapsulamento
         qualquer.setNome("Mama Uh"); // atribuição
         System.out.println(qualquer.getNome()); // saída de dados
+
+        Esportista jogadora = new Esportista();
+        Esportista surfista = new Esportista(123);
+        jogadora.setNome("Tuiks do futs");
+        System.out.println(jogadora.getNome());
+        //Polimorfismo
+        qualquer = new Esportista();
+        String resultado = qualquer.sacar();
+        System.out.println(resultado);
+        qualquer = new Pessoa();
+        resultado = qualquer.sacar();
+        System.out.println(resultado);
+
+
     }
 }
